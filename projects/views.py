e59c5e24a86d3784c8c37b555d01cedc45bfc562
Miguh Ruiz <miguhruiz@gmail.com>
2016-03-16 18:05:09 +0100

    Added views to represent project class on URL's

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from .models import Project
 
-# Create your views here.
+class CourseList(ListView):
+    model = Project
+
+class CourseDetail(DetailView):
+    model = Project
